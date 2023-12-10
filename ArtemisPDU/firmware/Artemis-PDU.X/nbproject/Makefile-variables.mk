d9f0e97cf2d828c8900020253e4360312cf8bf43
Ethan Chee <61472206+ethancheez@users.noreply.github.com>
2023-12-10 00:33:29 -1000

    Update manifest

--- ArtemisPDU/firmware/Artemis-PDU.X/nbproject/Makefile-variables.mk
+++ ArtemisPDU/firmware/Artemis-PDU.X/nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=Artemis-PDU.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/Artemis-PDU.X.production.hex
+CND_ARTIFACT_NAME_default=Artemis-PDU.X.production.null
+CND_ARTIFACT_PATH_default=dist/default/production/Artemis-PDU.X.production.null
